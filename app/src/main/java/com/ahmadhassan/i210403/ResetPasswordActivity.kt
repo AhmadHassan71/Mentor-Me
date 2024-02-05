7a8c71598b128739a007e2c53b2288a954825a98
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2024-02-05 14:36:17 +0500

    create Reset Password

--- app/src/main/java/com/ahmadhassan/i210403/ResetPasswordActivity.kt
+++ app/src/main/java/com/ahmadhassan/i210403/ResetPasswordActivity.kt
@@ -1,6 +1,7 @@
 package com.ahmadhassan.i210403
 
 
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -8,30 +9,30 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class ResetPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.forgotpassword)
+        setContentView(R.layout.resetpassword)
 
         val loginButton: ImageView = findViewById(R.id.backButton)
         loginButton.setOnClickListener {
             // Start the HomeActivity when the login button is clicked
-            val intent = Intent(this@ForgotPasswordActivity, LoginActivity::class.java)
+            val intent = Intent(this@ResetPasswordActivity, ForgotPasswordActivity::class.java)
             startActivity(intent)
         }
 
         val sendButton: Button = findViewById(R.id.sendButton)
         sendButton.setOnClickListener {
             // Start the HomeActivity when the login button is clicked
-            val intent = Intent(this@ForgotPasswordActivity, ResetPasswordActivity::class.java)
+            val intent = Intent(this@ResetPasswordActivity, LoginActivity::class.java)
             startActivity(intent)
         }
 
         val loginTextView: TextView = findViewById(R.id.LoginText)
         loginTextView.setOnClickListener {
             // Start the HomeActivity when the login button is clicked
-            val intent = Intent(this@ForgotPasswordActivity, LoginActivity::class.java)
+            val intent = Intent(this@ResetPasswordActivity, LoginActivity::class.java)
             startActivity(intent)
         }
     }
