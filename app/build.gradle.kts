f154e4a1c82fda2a1d85a5ddafc25e04d4a32675
Ahmad Hassan <ahmad.liberty9999@GMAIL.COM>
2024-03-10 14:30:53 +0500

    connect the database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 android {
     namespace = "com.ahmadhassan.i210403"
@@ -45,6 +46,7 @@ dependencies {
 
     // searechbar widget
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/ahmadhassan/i210403/RegisterActivity.kt
+++ app/src/main/java/com/ahmadhassan/i210403/RegisterActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
@@ -12,20 +13,29 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.database
 
 class RegisterActivity : AppCompatActivity() {
     private var isCitySelected = false
+    private lateinit var databaseRef: DatabaseReference
+    private lateinit var database: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
+        database = FirebaseDatabase.getInstance()
+        databaseRef = database.reference
+
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
 
-        val loginButton: Button = findViewById(R.id.LoginButton)
-        loginButton.setOnClickListener {
-            // Start the HomeActivity when the login button is clicked
-            val intent = Intent(this@RegisterActivity, VerifyActivity::class.java)
-            startActivity(intent)
-        }
+//        val loginButton: Button = findViewById(R.id.LoginButton)
+//        loginButton.setOnClickListener {
+//            // Start the HomeActivity when the login button is clicked
+//            val intent = Intent(this@RegisterActivity, VerifyActivity::class.java)
+//            startActivity(intent)
+//        }
 
         val spinner: Spinner = findViewById(R.id.CityEditText)
         val spinner2: Spinner = findViewById(R.id.CountryEditText)
@@ -45,6 +55,12 @@ class RegisterActivity : AppCompatActivity() {
             cities
         )
 
+        val name = findViewById<EditText>(R.id.NameEditText).toString()
+        val email = findViewById<EditText>(R.id.EmailEditText).toString()
+        val password = findViewById<EditText>(R.id.PasswordEditText).toString()
+        val contact = findViewById<EditText>(R.id.ContactEditText).toString()
+        var city = ""
+        var country = ""
 
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -68,6 +84,7 @@ class RegisterActivity : AppCompatActivity() {
                         "Selected city: $selectedCity",
                         Toast.LENGTH_SHORT
                     ).show()
+                    city = selectedCity
 
                     isCitySelected = true
 
@@ -119,7 +136,7 @@ class RegisterActivity : AppCompatActivity() {
                         "Selected country: $selectedCountry",
                         Toast.LENGTH_SHORT
                     ).show()
-
+                    country = selectedCountry
                     isCitySelected = true
 
                     val updatedCountries = countries.filter { it != "Enter Country" }.toTypedArray()
@@ -139,5 +156,26 @@ class RegisterActivity : AppCompatActivity() {
 
         }
 
+
+        val signUpButton: Button = findViewById(R.id.SignUpButton)
+        signUpButton.setOnClickListener {
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty() || contact.isEmpty() || city.isEmpty() || country.isEmpty()) {
+                // Check if any field is empty
+                Toast.makeText(this@RegisterActivity, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                // Check if email is in correct format using regex
+                Toast.makeText(this@RegisterActivity, "Invalid email format", Toast.LENGTH_SHORT).show()
+            } else {
+                val user = User(name, email, password, contact, city, country)
+                databaseRef.child("Users").child(contact).setValue(user)
+                val intent = Intent(this@RegisterActivity, VerifyActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+
+
     }
 }
+
+data class User(val name: String, val email: String, val password: String, val contact: String, val city: String, val country: String)
